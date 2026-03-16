d26cfe0586dae5f15a3ed8e49003df50b1eae8ed
anushka-1408 <atiwar55@asu.edu>
2026-03-15 22:44:47 -0700

    added store file that will see similar meanings

---
+++ app/store.py
@@ -0,0 +1,48 @@
+from typing import List, Optional
+from sentence_transformers import SentenceTransformer
+import chromadb
+
+from app.config import(EMBEDDING_MODEL_NAME, CHROMA_PERSIST_DIR, COLLECTION_NAME, RETRIEVAL_K)
+from app.config import Document
+
+class VectorStore:
+    def __init__(self):
+        print("loading model...")
+        self.embedder =SentenceTransformer(EMBEDDING_MODEL_NAME)
+        print("model loaded")
+
+        self.client = chromadb.PersistentClient(path=str(CHROMA_PERSIST_DIR))
+        self.collection = self.client.get_or_create_collection(name=COLLECTION_NAME, metadata={"hnsw:space": "cosine"})
+
+    def add_documents(self, documents: List[Document]) -> int:
+        if not documents:
+            return 0
+        
+        texts = [doc.content for doc in documents]
+        metadata = [doc.metadata for doc in documents]
+
+        ids = [f"doc_{i}_{doc.metadata['source']}" for i, doc in enumerate(documents)]
+
+        embeddings = self.embedder.encode(texts).tolist()
+
+        self.collection.add(ids = ids, embeddings=embeddings, documents=texts, metadata=metadata)
+        return len(documents)
+
+    def search(self, query: str, k: int = RETRIEVAL_K)-> List[Document]:
+        query_embedding = self.embedder.encode(query).tolist()
+
+        results = self.collection.query(query_embeddings =[query_embedding], n_results = k)
+
+        documents = []
+        for i in range(len(results["ids"][0])):
+            doc = Document(content=results['documents'][0][i], metadata=results['metadata'][0][i])
+            documents.append(doc)
+        return documents
+
+    def clear(self):
+        self.client.delete_collection(name=COLLECTION_NAME)
+        self.collection = self.client.get_or_create_collection(name=COLLECTION_NAME, metadata={"hnsw:space": "cosine"})
+
+    def count(self)-> int:
+        return self.collection.count()
+        
